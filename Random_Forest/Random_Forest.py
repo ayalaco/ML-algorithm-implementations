d432f812f42823fe3eb2b69b9a87b77e576f996c
ayalaco <77512548+ayalaco@users.noreply.github.com>
2022-05-08 20:17:37 +0300

    Add files via upload

---
+++ Random_Forest/Random_Forest.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from Tree import Node
+
+
+class RandomForest:
+
+    def __init__(self, dataset, labels, size_subsample, num_rand_features, num_trees, max_depth):
+
+        self.dataset = dataset
+        self.labels = labels
+        self.size_subsample = size_subsample
+        self.num_rand_features = num_rand_features
+        self.num_trees = num_trees
+        self.max_depth = max_depth
+        self.trees = None
+
+    def create_subsample(self):
+        """
+        generate random subsample
+        """
+        sub_data = self.dataset.sample(n=self.size_subsample, replace=True)
+        sub_labels = self.labels[sub_data.index]
+
+        return sub_data, sub_labels
+
+    def build_forest(self):
+        """
+        populate the forest with trees built based on different random sub-samples
+        """
+        self.trees = []
+
+        for tree in range(self.num_trees):
+            # generate random subsample from the dataset
+            subsample_x, subsample_y = self.create_subsample()
+
+            # build a tree for the subset:
+            root = Node(subsample_x, subsample_y, depth=0, num_rand_features=self.num_rand_features, forest=True)
+            root.build_tree(self.max_depth)
+
+            self.trees.append(root)
+
+    def predict(self, X_test):
+        """
+        predict an outcome for all trees, then choose the most common prediction.
+        """
+        predictions = pd.DataFrame(index=X_test.index, columns=[f'Tree {i}' for i in range(1, len(self.trees) + 1)])
+
+        # Get a prediction from each tree in the forest
+        for col, root in enumerate(self.trees):
+            predictions.iloc[:, col] = root.predict(X_test)
+
+        # choose the most common prediction
+        prediction_mode = predictions.mode(axis=1)
+
+        # deal with 50-50 probabilities (won't happen for on odd number of trees):
+        if prediction_mode.shape[1] == 2:
+            ind_ambig = prediction_mode[1].notnull()
+            prediction_mode.iloc[ind_ambig, 0] = 'M'  # better to have a false positive in this case
+            prediction_mode.drop(columns=1, inplace=True)
+
+        return prediction_mode.squeeze()
---
+++ Random_Forest/main.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from Random_Forest import RandomForest
+
+if __name__ == '__main__':
+    # get data
+    data = pd.read_csv('wdbc.data')
+    data.columns = ['ID number', 'Diagnosis'] + [f'feature{i}' for i in range(1, data.shape[1] - 1)]
+
+    X = data.iloc[:, 2:]
+    y = data.iloc[:, 1]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # build a forest and predict
+    size_subsample = int(np.floor(len(y_train) / 3))
+    num_rand_features = int(np.floor(np.sqrt(x_train.shape[1])))
+    num_trees = 50
+    max_depth = 2
+
+    forest = RandomForest(x_train, y_train, size_subsample, num_rand_features, num_trees, max_depth)
+    forest.build_forest()
+
+    predictions = forest.predict(x_test)
+
+    accuracy = np.sum(predictions == y_test) / predictions.shape[0]
+    print(f"Decision tree accuracy is {accuracy * 100:2.4}%")
